247f7f06cb1e42f1456aefcb8f949a90543f5a4d
Blaise <jetblaise@gmail.com>
2024-04-17 10:58:29 -0400

    Refactor AppHeader component imports

--- src/view/chrome/AppHeader.tsx
+++ src/view/chrome/AppHeader.tsx
@@ -1,5 +1,13 @@
 import { BiLogoGithub } from "react-icons/bi";
-import { VuiFlexContainer, VuiFlexItem, VuiTitle, VuiIconButton, VuiIcon, VuiLink, VuiAppHeader } from "../../ui";
+import {
+  VuiFlexContainer,
+  VuiFlexItem,
+  VuiTitle,
+  VuiIconButton,
+  VuiIcon,
+  VuiLink,
+  VuiAppHeader,
+} from "../../ui";
 import { HeaderLogo } from "./HeaderLogo";
 
 export const AppHeader = () => {
@@ -15,8 +23,8 @@ export const AppHeader = () => {
             <VuiTitle size="xs">
               <h1>
                 <strong>
-                  <VuiLink isAnchor href="https://vectara.github.io/create-ui">
-                    Vectara Create-UI
+                  <VuiLink isAnchor href="https://longevity.so">
+                    Longevity Copilot
                   </VuiLink>
                 </strong>
               </h1>
@@ -26,7 +34,7 @@ export const AppHeader = () => {
       }
       right={
         <VuiIconButton
-          href="https://github.com/vectara/create-ui"
+          href="https://github.com/langlabsio/longevity-copilot"
           target="_blank"
           color="neutral"
           size="l"

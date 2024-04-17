7a0ca6433faa71b333aa84257fa4caf945896e66
Blaise <jetblaise@gmail.com>
2024-04-17 10:20:58 -0400

    init

---
+++ src/configuration.ts
@@ -0,0 +1,10 @@
+import { Config } from "./contexts/ConfigurationContext";
+
+export const configuration: Config = {
+  corpusId: "2",
+  customerId: "1600513235",
+  appTitle: "longevity-copilot",
+  apiKey: "zut_X2Xk0-2HXGS90_mRM9xJSWftoVOpqTH04nyhzw",
+  endpoint: "api.vectara.io",
+  questions: ["What are the most important lifestyle changes I can make to increase my longevity?","How significant is diet in determining my life expectancy, and what dietary approach is considered best for longevity?","Can you explain the role of exercise in longevity and what types of physical activity are most beneficial?","How does sleep impact longevity, and what are the best practices for improving sleep quality?","What are the latest advancements in anti-aging research, and how close are we to significantly extending human lifespan?"],
+};
